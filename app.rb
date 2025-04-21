9f17995fc3731a94f992928cbec3714ff4bf49a3
varshnimurali <vmurali0@chicagobooth.edu>
2025-04-21 22:19:58 +0000

    save updates

--- app.rb
+++ app.rb
@@ -15,8 +15,8 @@ get("/") do
   erb(:homepage)
 end
 
-get("/:first_symbol") do
-  @the_symbol = params.fetch("first_symbol")
+get("/:from_currency") do
+  @the_symbol = params.fetch("from_currency")
 
   @url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}"
 
@@ -29,4 +29,22 @@ get("/:first_symbol") do
   @currencies = @parsed_response.fetch("currencies")
 
   erb(:step_one)
+end
+
+
+get("/:from_currency/:to_currency") do
+  @from = params.fetch("from_currency")
+  @to = params.fetch("to_currency")
+
+  @url = "https://api.exchangerate.host/convert?from=#{@from}&to=#{@to}&amount=1&access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  
+  @raw_response = HTTP.get(@url)
+  
+  @string_response = @raw_response.to_s 
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @amount = @parsed_response.fetch("result")
+  
+  erb(:step_two)
 end
